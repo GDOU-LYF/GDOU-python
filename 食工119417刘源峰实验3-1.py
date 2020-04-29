4debf9f1522e2d78416aef872a1fe56a2c3b1473
LYF <liuyuanfeng1@stu.gdou.edu.cn>
2020-04-29 12:41:51 +0800

    4-29

--- 食工119417刘源峰实验3-1.py
+++ 食工119417刘源峰实验3-1.py
@@ -5,13 +5,14 @@ else:
     tures=False
 if tures:
     string="The triangle being made up of a, b and c is "
-    if a==b==c:
+    if a==b==c:#等边三角形
         print(string+"equilateral triangle")
-    elif a==b or a==c or b==c:
-        print(string+"isosceles spherical triangle")
     elif a**2+b**2==c**2 or a**2+c**2==b**2 or b**2+c**2==a**2:
+        #直角三角形
         print(string+"right angled triangle")
-    else:
+    elif a==b or a==c or b==c: #等腰三角形 
+            print(string+"isosceles spherical triangle")
+    else:#其他三角形
         print(string+"ordinary triangle")
-else:
-    print("No triangle!")
+else:#不是三角形
+    print("Not a triangle!")
--- 食工119417刘源峰实验3-2.py
+++ 食工119417刘源峰实验3-2.py
@@ -1,4 +1,4 @@
-x=int(eval(input("请输入x:")))
+x=float(eval(input("请输入x:")))
 if x<0 and x!=-3:
     ret=x**2+x-6
 elif 0<=x<10 and x!=2 and x!=3:
