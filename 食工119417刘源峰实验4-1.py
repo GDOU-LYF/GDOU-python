2593a28c574539b9d8b885fa10f740d470cf34cc
LYF <liuyuanfeng1@stu.gdou.edu.cn>
2020-05-13 11:14:29 +0800

    5-13

--- 食工119417刘源峰实验4-1.py
+++ 食工119417刘源峰实验4-1.py
@@ -1,5 +1,5 @@
 #1,3,5,7 组成互不相同的三位数,每行6个
-a=['1','3','5','7']
+a=list("1357")
 count=0
 for i in a:
     for j in a:
@@ -10,3 +10,16 @@ for i in a:
                 if count==6:
                     count=0
                     print()
+
+# print(len(a),min(a),max(a))
+# print(a.index('3'))
+# print(a.count('5'))
+# b=[item*2 for item in map(eval,a)]
+# print(b)
+# c=[i*2 for i in map(eval,a)   if i>2]
+# print(c)
+# ls=list("ABC")
+# ls.insert(1,'z')
+# ls.reverse()
+# ls.remove('z')
+# print(ls)
