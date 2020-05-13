2754615d9f4d4c57c516672929af9bc6fed18da4
LYF <liuyuanfeng1@stu.gdou.edu.cn>
2020-05-13 10:17:18 +0800

    5-13 优化

--- 食工119417刘源峰实验4-2.py
+++ 食工119417刘源峰实验4-2.py
@@ -2,10 +2,15 @@
 #依据题目意思 5a+3b+(1/3)c=100
 #应该用3重循环,改进:
 M=100
-a,b,c=0,0,0
 print("鸡翁\t鸡母\t鸡雏")
-for i in range(0,21):
-    for j in range(0,33):
-        for k in range(0,300):
-            if i*15+j*9+k==300 and i+j+k==100:
-                print("{}\t{}\t{}".format(i,j,k))
+# for i in range(0,21):#方法一
+#     for j in range(0,33):
+#         for k in range(0,300):
+#             if i*15+j*9+k==300 and i+j+k==100:
+#                 print("{}\t{}\t{}".format(i,j,k))
+
+for a in range(0,M+1):#优化
+    for b in range(0,M+1):
+        c=M-a-b
+        if 15*a+9*b+c==M*3:
+            print("{}\t{}\t{}".format(a,b,c))
