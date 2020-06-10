fe5259f9f6075c0604fba19edebcfb7ccee4e5b0
LYF <liuyuanfeng1@stu.gdou.edu.cn>
2020-06-10 11:23:11 +0800

    简化字符串逆序

--- 食工119417刘源峰实验5-2.py
+++ 食工119417刘源峰实验5-2.py
@@ -1,9 +1,10 @@
 a="Python"
-b=",".join(a).split(",")
-
+# b=list(a)
+b=""
 for i in range(1,len(a)+1):
-    b[i-1]=a[-i]
-a=b.copy()
+    # b[i-1]=a[-i]
+    b+=a[-i]
+a=b
 
 for i in a:
     print("{}".format(i),end="")
